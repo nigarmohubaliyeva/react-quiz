cd2560529b676e8377dbeb842d9c5768f789cd70
Nigar Mohubaliyeva <nigarmohubaliyeva@gmail.com>
2023-08-31 17:07:36 +0400

    ✨ added components

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useReducer } from "react";
+
 import Header from "./Header";
 import Main from "./Main";
 import Loader from "./Loader";
@@ -6,84 +7,142 @@ import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
+import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
+import Footer from "./Footer";
+import Timer from "./Timer";
+
+const SECS_PER_QUESTION = 30;
 
 const initialState = {
   questions: [],
-  status: 'loading',
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'dataReceived':
+    case "dataReceived":
       return {
         ...state,
         questions: action.payload,
         status: "ready",
       };
-    case 'dataFailed':
+    case "dataFailed":
       return {
         ...state,
         status: "error",
-      }
-    case 'start':
-      return { ...state, status: "active" }
-    case 'newAnswer':
+      };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
+      };
+    case "newAnswer":
       const question = state.questions.at(state.index);
 
       return {
         ...state,
         answer: action.payload,
-        point:
+        points:
           action.payload === question.correctOption
             ? state.points + question.points
             : state.points,
       };
     case "nextQuestion":
-      return { ...state, index: state.index + 1 }
+      return { ...state, index: state.index + 1, answer: null };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+    case "restart":
+      return { ...initialState, questions: state.questions, status: "ready" };
+
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
+
     default:
-      throw new Error('Action is unknown')
+      throw new Error("Action unkonwn");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer
-    (reducer, initialState)
+  const [
+    { questions, status, index, answer, points, highscore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
 
   useEffect(function () {
-    fetch('http://localhost:9000/questions')
+    fetch("http://localhost:9000/questions")
       .then((res) => res.json())
-      .then((data) => dispatch({
-        type: 'dataReceived',
-        payload: data
-      }))
-      .catch((err) => dispatch({ type: 'dataFailed' }));
-  }, [])
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
 
   return (
     <div className="app">
       <Header />
 
       <Main>
-        {status === 'loading' && <Loader />}
-        {status === 'error' && <Error />}
-        {status === 'ready' &&
-          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
-        {status === 'active' && (
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
           <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
             <Question
               question={questions[index]}
               dispatch={dispatch}
-              answer={answer} />
-            <NextButton dispatch={dispatch} answer={answer} />
+              answer={answer}
+            />
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              <NextButton
+                dispatch={dispatch}
+                answer={answer}
+                numQuestions={numQuestions}
+                index={index}
+              />
+            </Footer>
           </>
         )}
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+            dispatch={dispatch}
+          />
+        )}
       </Main>
-
     </div>
   );
 }
--- src/components/Question.js
+++ src/components/Question.js
@@ -4,7 +4,10 @@ function Question({ question, dispatch, answer }) {
     return (
         <div>
             <h4>{question.question}</h4>
-            <Options question={question} answer={answer} dispatch={dispatch} />
+            <Options
+                question={question}
+                answer={answer}
+                dispatch={dispatch} />
         </div>
     );
 }
